6c7777f5ef3376758f93ac86f1260a3549fa2389
unknown <yusquen22@gmail.com>
2024-07-01 20:07:41 -0500

    Request object

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__) #Refering the server flask to this archive
 
 @app.route("/")
-def hello():
-    return "Hello world"+{}
+def hello():              #Request object for pbtaining all client information  
+    user_ip_information = request.remote_addr
+    return f"Hows you doing, Your IP adress is: {user_ip_information}"
 
 app.run(host='0.0.0.0' , port=5000, debug='True') # Accesing from anywhere adress ip on app on the port..
                                     #activate to show errors in development not in productions
