18f9f76472dba83620b09f0b229aa8872a2959e3
unknown <yusquen22@gmail.com>
2024-07-01 19:53:26 -0500

    Making debugging in Flask

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__) #Refering the server flask to this archive
 
 @app.route("/")
 def hello():
-    return "Hello world"
+    return "Hello world"+{}
 
-app.run(host='0.0.0.0' , port=5000) # Accesing from anywhere adress ip on app on the port..
-
+app.run(host='0.0.0.0' , port=5000, debug='True') # Accesing from anywhere adress ip on app on the port..
+                                    #activate to show errors in development not in productions
