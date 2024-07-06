eecbd9953e5ddbfb7368191e5e5f00e309720ecd
unknown <yusquen22@gmail.com>
2024-07-06 11:56:12 -0500

    Cycles and Decision structures in Jinja

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__) #Refering the flask server to this archive
 
+items =["ITEM1", "ITEM2", "ITEM3", "ITEM4" ]
 @app.route("/inde")
 def index():              #Request object for obtaining all client information  
     user_ip_information = request.remote_addr
@@ -12,7 +13,11 @@ def index():              #Request object for obtaining all client information
 @app.route("/show_information_address")
 def show_information():
     user_ip = request.cookies.get("user_ip_information")
-    return render_template("ip_information.html", user_ip=user_ip) 
+    context = {
+        "user_ip": user_ip,
+        "items":items
+     }                                           #context=context           
+    return render_template("ip_information.html", **context) 
 
 app.run(host='0.0.0.0' , port=5000, debug='True') # Accesing from anywhere adress ip on app on the port..
                                     #activate to show errors in development else not in productions
