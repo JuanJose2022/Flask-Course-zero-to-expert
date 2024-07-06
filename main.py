5092bb4d46bade9abef4bb0373929cbbdd601882
unknown <yusquen22@gmail.com>
2024-07-06 10:40:46 -0500

    Rendering html templates with Jinja

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
-app = Flask(__name__) #Refering the server flask to this archive
+app = Flask(__name__) #Refering the flask server to this archive
 
 @app.route("/inde")
-def index():              #Request object for pbtaining all client information  
+def index():              #Request object for obtaining all client information  
     user_ip_information = request.remote_addr
     response = make_response(redirect("/show_information_address"))
     response.set_cookie("user_ip_information", user_ip_information)
@@ -12,8 +12,8 @@ def index():              #Request object for pbtaining all client information
 @app.route("/show_information_address")
 def show_information():
     user_ip = request.cookies.get("user_ip_information")
-    return f"Hows you doing, your IP adress is: {user_ip}"
+    return render_template("ip_information.html", user_ip=user_ip) 
 
 app.run(host='0.0.0.0' , port=5000, debug='True') # Accesing from anywhere adress ip on app on the port..
-                                    #activate to show errors in development not in productions
+                                    #activate to show errors in development else not in productions
 
