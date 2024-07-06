7d70de81dc4fd4c25b6c969f76de33704c22c199
unknown <yusquen22@gmail.com>
2024-07-06 09:56:07 -0500

    Object request and response flow

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__) #Refering the server flask to this archive
 
-@app.route("/")
-def hello():              #Request object for pbtaining all client information  
+@app.route("/inde")
+def index():              #Request object for pbtaining all client information  
     user_ip_information = request.remote_addr
-    return f"Hows you doing, Your IP adress is: {user_ip_information}"
+    response = make_response(redirect("/show_information_address"))
+    response.set_cookie("user_ip_information", user_ip_information)
+    return response
+
+@app.route("/show_information_address")
+def show_information():
+    user_ip = request.cookies.get("user_ip_information")
+    return f"Hows you doing, your IP adress is: {user_ip}"
 
 app.run(host='0.0.0.0' , port=5000, debug='True') # Accesing from anywhere adress ip on app on the port..
                                     #activate to show errors in development not in productions
+
