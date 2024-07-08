a727afbd9cced73178c8bbaf524c3e5d087906c0
unknown <yusquen22@gmail.com>
2024-07-07 19:39:16 -0500

    Reusing static templates using INCLUDE

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def index():              #Request object for obtaining all client information
     response = make_response(redirect("/show_information_address"))
     response.set_cookie("user_ip_information", user_ip_information)
     return response
-
 @app.route("/show_information_address")
 def show_information():
     user_ip = request.cookies.get("user_ip_information")
@@ -18,7 +17,5 @@ def show_information():
         "items":items
      }                                           #context=context           
     return render_template("ip_information.html", **context) 
-
 app.run(host='0.0.0.0' , port=5000, debug='True') # Accesing from anywhere adress ip on app on the port..
                                     #activate to show errors in development else not in productions
-
