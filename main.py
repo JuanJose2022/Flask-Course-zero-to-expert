f5756c8a24c60432cd577f2b039160727ccbfe25
unknown <yusquen22@gmail.com>
2024-07-27 15:27:53 -0500

    Bootstrap Integration with Flask Flask bootstrap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__) #Refering the flask server to this archive
+bootstrap = Bootstrap(app)
+
 
 items =["rice", "bread", "pupetter", "tomate" ]
 
