e81d8ad7fdfeab25ded47c97f90fe3e1af7e2053
unknown <yusquen22@gmail.com>
2024-07-29 16:13:37 -0500

    Adding configuration to Flask: Encrypted, enviroment variables

---
+++ config.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+#export FLASK_ENV=$1  
+export FLASK_ENV=$1
+echo "FLASK_ENV set to $FLASK_ENV"
+
+# On terminal: bash -c "source ./config.sh development"
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__) #Refering the flask server to this archive
 bootstrap = Bootstrap(app)
-
+app.config["SECRET_KEY"]= 'CLAVE SECRETA' #Making encryption our cookie ip, utilizing python session 
 
 items =["rice", "bread", "pupetter", "tomate" ]
 
@@ -15,15 +15,18 @@ def not_found_endpoint(error):
 def index():              #Request object for obtaining all client information  
     user_ip_information = request.remote_addr
     response = make_response(redirect("/show_information_address"))
-    response.set_cookie("user_ip_information", user_ip_information)
+   #response.set_cookie("user_ip_information", user_ip_information)
+    session["user_ip_information"] = user_ip_information   #Encrypted
     return response
+
 @app.route("/show_information_address")
 def show_information():
-    user_ip = request.cookies.get("user_ip_information")
+   #user_ip = request.cookies.get("user_ip_information") 
+    user_ip = session.get("user_ip_information")
     context = {
         "user_ip": user_ip,
         "items":items
      }                                           #context=context           
     return render_template("ip_information.html", **context) 
-app.run(host='0.0.0.0' , port=5000, debug='True') # Accesing from anywhere adress ip on app on the port..
+app.run(host='0.0.0.0' , port=5000, debug='True') # Accesing from anywhere ip adress on our app on the port..
                                     #activate to show errors in development else not in productions
