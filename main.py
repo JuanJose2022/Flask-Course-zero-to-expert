7864ed3fb9ef80c84d10df40093d72ec6c57ba38
unknown <yusquen22@gmail.com>
2024-08-01 21:29:42 -0500

    Forms with Flask wtf

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__) #Refering the flask server to this archive
 bootstrap = Bootstrap(app)
@@ -7,6 +10,11 @@ app.config["SECRET_KEY"]= 'CLAVE SECRETA' #Making encryption our cookie ip, util
 
 items =["rice", "bread", "pupetter", "tomate" ]
 
+
+class LoginForm(FlaskForm):
+    username = StringField("User Name: ", validators=[DataRequired()])
+    password = PasswordField("Password: ", validators=[DataRequired()])
+    submit = SubmitField("Send Data: ", validators=[DataRequired()])
 @app.errorhandler(404)
 def not_found_endpoint(error):
     return render_template('404.html', error=error)
@@ -23,9 +31,11 @@ def index():              #Request object for obtaining all client information
 def show_information():
    #user_ip = request.cookies.get("user_ip_information") 
     user_ip = session.get("user_ip_information")
+    login_form = LoginForm()
     context = {
         "user_ip": user_ip,
-        "items":items
+        "items":items,
+        "login_form":login_form
      }                                           #context=context           
     return render_template("ip_information.html", **context) 
 app.run(host='0.0.0.0' , port=5000, debug='True') # Accesing from anywhere ip adress on our app on the port..
